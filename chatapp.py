a126cbe56bdf5c749ec3dd7c34da960ddc4a8968
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-11-12 16:56:00 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -71,7 +71,15 @@ def chatbot_response(text):
     return res
 
 #Creating GUI
-
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 #c=st.container()
 if 'chat' not in st.session_state:
     st.session_state.chat = ""
