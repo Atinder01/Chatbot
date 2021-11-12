513dae6673ae5af406a2a4a445e66ecf5ee4de67
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-11-12 16:27:37 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -81,8 +81,7 @@ if 'chat' not in st.session_state:
 def update_first():
     res = chatbot_response(st.session_state.first)
     st.session_state.chat = st.session_state.chat + os.linesep + "\n__You__: " + st.session_state.first + os.linesep + "\n__Bot__: " + res
-    t=st.session_state.chat
-    st.write(t)
+    st.write(st.session_state.chat)
 
 st.sidebar.image('logo.PNG')
 st.sidebar.text_input(label='Chat with me', key='first', on_change=update_first)
