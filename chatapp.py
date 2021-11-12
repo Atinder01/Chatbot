6d3a5e5a690c7bde54e5bec6cbeeb49b7fe3b216
Atinderpal Kaur <67895402+Atinder01@users.noreply.github.com>
2021-11-12 16:11:10 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -72,14 +72,14 @@ def chatbot_response(text):
 
 #Creating GUI
 st.title('LiveChat Bot')
-
+c=st.container()
 if 'chat' not in st.session_state:
     st.session_state.chat = ""
    
 def update_first():
     res = chatbot_response(st.session_state.first)
     st.session_state.chat = st.session_state.chat + os.linesep + "\n__You__: " + st.session_state.first + os.linesep + "\n__Bot__: " + res
-    st.write(st.session_state.chat)
+    c.write(st.session_state.chat)
 
 st.sidebar.image('logo.PNG')
 st.sidebar.text_input(label='Chat with me', key='first', on_change=update_first)
